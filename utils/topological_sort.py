2df88b10f429eca9885bcfb9d64dff491f4870e4
chahal-navi <nnavi4806@gmail.com>
2025-07-06 17:42:20 +0530

    topological_sort.py

--- utils/topological_sort.py
+++ utils/topological_sort.py
@@ -10,3 +10,20 @@ def build_tree(v: 'Tensor', topo: List):
     for children in v.prev:
       build_tree(children, topo)
   topo.append(v)
+
+"""
+block diagonal matrix fucntion
+used to create the jacobian for matrix multiplication.
+for flattened marices
+"""
+
+def block_diag_matrix(matrix, shape):
+  block_diag_matrix = np.zeros(shape)
+  block_num = shape[0]/matrix.shape[0]
+  print(block_num, 'is the block number')
+  i = 0
+  while i < block_num:
+    block_diag_matrix[i*matrix.shape[0] : matrix.shape[0]*(i + 1), i*matrix.shape[1] : matrix.shape[1] * (i + 1)] = matrix
+    i += 1
+  return block_diag_matrix
+
