a3e4a51b6999dbade0087c02dd7a04ba27acd600
chahal-navi <nnavi4806@gmail.com>
2025-07-06 17:40:28 +0530

    Update backprop.py

--- utils/backprop.py
+++ utils/backprop.py
@@ -1,54 +1,18 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "ef62160f-d715-4399-a4c4-3380bef65cb1",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import numpy as np\n",
-    "\n",
-    "class backprop():\n",
-    "  \"\"\"\n",
-    "  this object will call a tarverser function defined in itself\n",
-    "  that fucntion will store all the jacobians of all the intermediate values\n",
-    "  which have requires_grad as True\n",
-    "  \"\"\"\n",
-    "  def __init__(self, v: 'Tensor'):\n",
-    "    self.node = v\n",
-    "    self.topological_tree = []\n",
-    "    build_tree(self.node, self.topological_tree)\n",
-    "    self.topological_tree.reverse()\n",
-    "    print(self.topological_tree)\n",
-    "    def propogator(self):\n",
-    "      self.node.jacobian = np.eye(self.node.flatten_shape)\n",
-    "      for i in self.topological_tree:\n",
-    "        i.grad_prop(self.node)\n",
-    "    propogator(self)\n",
-    "\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.1"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+class backprop():
+  """
+  this object will call a tarverser function defined in itself
+  that fucntion will store all the jacobians of all the intermediate values
+  which have requires_grad as True
+  """
+  def __init__(self, v: 'Tensor'):
+    self.node = v
+    self.topological_tree = []
+    build_tree(self.node, self.topological_tree)
+    self.topological_tree.reverse()
+    print(self.topological_tree)
+    def propogator(self):
+      self.node.jacobian = np.eye(self.node.flatten_shape)
+      for i in self.topological_tree:
+        i.grad_prop(self.node)
+    propogator(self)
+
